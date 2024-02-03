0f1e9b1402f218deeb297141b03b92205e4e70d3
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-03 10:28:26 +0100

    Show all data and add styles

--- src/components/QuoteList.jsx
+++ src/components/QuoteList.jsx
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
 import QuoteItem from "./QuoteItem";
 
-function QuoteList(quotes) {
+function QuoteList({quotes}) {
+const htmlQuotes = quotes.map ((quote, idx) => (
+<li key={idx} className="main__list--listItem">
+        <QuoteItem 
+        name={quote.character}
+        quote={quote.quote} />
+      </li>
+
+))
+
   return (
     <ul className="main__list">
-      <li className="main__list--listItem">
-        <QuoteItem name="Ross" quote="Pivot! Pivot! Pivot! Pivot! Pivot!" />
-      </li>
+      {htmlQuotes}
     </ul>
   );
 }
