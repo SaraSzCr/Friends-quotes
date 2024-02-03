ca89e103d72c6c10333d157f40f02e66ae787071
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-03 08:55:31 +0100

    add JSON file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import '../scss/App.scss'
+import "../scss/App.scss";
 
 function App() {
   return (
     <div>
-      Escribe aquí tu código
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
