23026166da3a69d4e440c78a8f3af8a1d7d6e10b
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-04-24 18:58:51 +0100

    add styles

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -12,26 +12,30 @@ function Filters({ handleFilter}) {
 
   return (
     <form className="main__form">
-      <label htmlFor="text">Filtrar por frase</label>
-      <input
-        className="main__form--filter"
-        type="text"
-        name="quote"
-        id="quote"
-        onInput={handleInputQuote}
-      />
-      <label htmlFor="character">
-        Filtrar por personaje
-        <select onInput={handleInputCharacter}>
-          <option value="all">Todos</option>
-          <option value="Monica">Monica</option>
-          <option value="Ross">Ross</option>
-          <option value="Chandler">Chandler</option>
-          <option value="Phoebe">Phoebe</option>
-          <option value="Rachel">Rachel</option>
-          <option value="Joey">Joey</option>
-        </select>
-      </label>
+      <section>
+        <label htmlFor="text">Filtrar por frase</label>
+        <input
+          className="main__form--filter"
+          type="text"
+          name="quote"
+          id="quote"
+          onInput={handleInputQuote}
+        />
+      </section>
+      <section>
+        <label htmlFor="character">
+          Filtrar por personaje
+          <select onInput={handleInputCharacter}>
+            <option value="all">Todos</option>
+            <option value="Monica">Monica</option>
+            <option value="Ross">Ross</option>
+            <option value="Chandler">Chandler</option>
+            <option value="Phoebe">Phoebe</option>
+            <option value="Rachel">Rachel</option>
+            <option value="Joey">Joey</option>
+          </select>
+        </label>
+      </section>
     </form>
 
     
